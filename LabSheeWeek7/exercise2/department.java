4adbe4c2276577409c36a754283750121b57643f
Gerard <Gerardosullivan.work@email.com>
2020-10-15 23:21:42 +0100

    Finished Lab 7

---
+++ LabSheeWeek7/exercise2/department.java
@@ -0,0 +1,39 @@
+package LabSheeWeek7.exercise2;
+
+import java.util.Arrays;
+
+public class department {
+    private String name;
+    private students student[];
+
+    public department(String name, students[] student) {
+        this.name = name;
+        this.student = student;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudent(students[] student) {
+        this.student = student;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public students[] getStudent() {
+        return student;
+    }
+
+    public String toString(){
+        String str = "\n\nName: " + getName() + "\nList of Students: \n";
+
+        for(int i=0;i<getStudent().length;i++)
+            if(student[i]!=null)
+                str+=student[i];
+
+        return str;
+    }
+}
